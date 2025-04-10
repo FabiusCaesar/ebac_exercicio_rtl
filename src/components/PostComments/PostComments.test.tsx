aec3f123b91556618cbbe60067e3d3a28c021410
fabius <fabioclf@gmail.com>
2025-04-10 18:38:58 -0400

    Adiciona teste para inserção de dois comentários com React Testing Library

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,23 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve adicionar dois comentários ao Post', () => {
+        render(<PostComment />)
+
+        const textarea = screen.getByTestId('textarea-coment')
+        const botao = screen.getByTestId('btn-enviar')
+
+        // Primeiro comentário
+        fireEvent.change(textarea, { target: { value: 'Comentário 1' } })
+        fireEvent.click(botao)
+
+        // Segundo comentário
+        fireEvent.change(textarea, { target: { value: 'Comentário 2' } })
+        fireEvent.click(botao)
+
+        // Verificações
+        expect(screen.getByText('Comentário 1')).toBeInTheDocument()
+        expect(screen.getByText('Comentário 2')).toBeInTheDocument()
+    })
 });
